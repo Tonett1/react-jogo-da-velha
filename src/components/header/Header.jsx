1b7d2a53c832549e340a8adcfad58afcdc0df325
Ryan Tonett <ryantonettferreira@gmail.com>
2023-08-29 02:16:05 -0300

    Criado componente de ícone

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,13 +1,17 @@
 import styles from './Header.module.css'
 
-import Title from "../title/Title"
-import Subtitle from "../subtitle/Subtitle"
+import Title from '../title/Title'
+import Subtitle from '../subtitle/Subtitle'
+import Icon from '../icon/Icon'
 
 function Header () {
   return (
     <div className={styles.header}>
       <Title>Jogo da Velha</Title>
-      <Subtitle>Criado por Ryan tonet</Subtitle>
+      <Subtitle>Criado por Ryan tonett</Subtitle>
+      <div className={styles.iconContent}>
+        <Icon iconName="github" link='https://github.com/Tonett1' />
+      </div>
     </div>
   )
 }
